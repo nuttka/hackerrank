6c6f73531102a85f2453ad24e7ec3f723665d87d
Lucas Augusto <nutkaugusto@gmail.com>
2022-01-24 09:40:50 -0300

    first commit

---
+++ 12hour.py
@@ -0,0 +1,42 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'timeConversion' function below.
+#
+# The function is expected to return a STRING.
+# The function accepts STRING s as parameter.
+#
+
+def timeConversion(s):
+    if s[8] == "P":
+        if int(s[:2]) < 12:
+            number = s[:2]
+            number = int(number) + 12
+            return str(number) + s[2:8]
+        else:
+            return s[:8]
+    else:
+        if int(s[:2]) < 12:
+            return s[:8]
+        else:
+            number = s[:2]
+            number = int(number) - 12
+            return '0' + str(number) + s[2:8] if len(str(number)) == 1 else str(number) + s[2:8]
+            
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    s = input()
+
+    result = timeConversion(s)
+
+    fptr.write(result + '\n')
+
+    fptr.close()
