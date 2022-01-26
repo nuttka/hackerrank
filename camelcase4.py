2b6f6a68b211b74518d5eff464c6608ce36cb089
Lucas Augusto <nutkaugusto@gmail.com>
2022-01-26 08:51:50 -0300

    camelcase

--- camelcase4.py
+++ camelcase4.py
@@ -1,4 +1,42 @@
-# import fileinput
+import fileinput
+import re
 
-# for line in fileinput.input():
-#     pass
+for line in fileinput.input():
+    _op, _type, _string = line.rstrip().split(';')
+
+    if _op == 'S':
+
+        if _type == 'M':
+            words = _string[:-2]
+            s = re.sub(r"(\w)([A-Z])", r"\1 \2", words)
+            print(s.lower())
+
+        elif _type == 'C':
+            s = re.sub(r"(\w)([A-Z])", r"\1 \2", _string)
+            print(s.lower())
+
+            
+        elif _type == 'V':
+            s = re.sub(r"(\w)([A-Z])", r"\1 \2", _string)
+            print(s.lower())
+
+
+
+    elif _op == 'C':
+        words = _string.title()
+
+        if _type == 'M':
+            words = re.sub(' ', '', words)
+
+            print(words[0].lower() + words[1:] + "()")
+
+        elif _type == 'C':
+            words = re.sub(' ', '', words)
+
+            print(words)
+            
+            
+        elif _type == 'V':
+            words = re.sub(' ', '', words)
+
+            print(words[0].lower() + words[1:])
