103acbc401386a9ef0db18f52896b2a80fc50eae
Lucas Augusto <nutkaugusto@gmail.com>
2022-04-04 19:43:22 -0300

    setup

---
+++ templ.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+#define _                        \
+   ios_base::sync_with_stdio(0); \
+   cin.tie(0);
+#define endl '\n'
+
+#define MAX 10000
+
+using namespace std;
+
+int main() {
+   _;
+
+   return 0;
+}
